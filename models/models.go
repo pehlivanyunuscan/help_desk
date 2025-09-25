19997f819cd7cc9a7198c66acddd2adaa22fa309
pehlivanyunuscan <pehlivanyunuscan@hotmail.com>
2025-09-25 15:03:52 +0300

    refactor: move request/response structs to models package

--- models/models.go
+++ models/models.go
@@ -31,3 +31,22 @@ type FaultReport struct {
 	UpdatedAt       time.Time      `json:"updated_at"`
 	DeletedAt       gorm.DeletedAt `gorm:"index" json:"-"`
 }
+
+// Gelen istek için kullanılacak yapı
+type CreateFaultReportRequest struct {
+	Title           string `json:"title"`
+	UserDescription string `json:"user_description"`
+	Clock           int64  `json:"clock"`
+	MachineID       string `json:"machine_id"`
+	Asset           string `json:"asset"`
+}
+
+// API yanıtı için kullanılacak yapı
+type FaultReportResponse struct {
+	Priority        string    `json:"priority"`
+	Asset           string    `json:"asset"`
+	Title           string    `json:"title"`
+	Duration        string    `json:"duration"`
+	Timestamp       time.Time `json:"timestamp"`
+	UserDescription string    `json:"user_description"`
+}
